bff0ec8b456b64e738e2038eece5d89c67cbfd98
hieunekkkkkk <hieutmhe180168@fpt.edu.vn>
2025-06-21 11:33:42 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const connectDB = require('./src/config/database');
 const router = require('./src/routes/index');
 const adminRoutes = require('./src/routes/admin');
 const authRoutes = require('./src/routes/auth');
-const paymentRoutes = require('./src/routes/payment.routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./src/swagger/swaggerConfig');
 const { metricsMiddleware, metricsEndpoint } = require('./src/middleware/metrics');
@@ -20,6 +19,7 @@ app.use(cors({
 app.use(express.json({ limit: '1gb' }));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 // Metrics middleware
 app.use(metricsMiddleware);
 app.get('/metrics', metricsEndpoint);
@@ -32,11 +32,7 @@ connectDB();
 app.use('/api', router);
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
-// app.use('/api/payment', paymentRoutes);
-// Cấu hình CORS
-app.use(cors({
-    origin: 'http://localhost:3000', // URL của frontend
-    credentials: true
-}));
+
+
 
 module.exports = app;
